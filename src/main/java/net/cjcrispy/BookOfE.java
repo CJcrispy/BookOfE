b6b04dbdfef197fd9707051920e25cfeafe22cd0
CJcrispy <49406764+CJcrispy@users.noreply.github.com>
2025-01-19 10:56:14 -0500

    Initial commit

---
+++ src/main/java/net/cjcrispy/BookOfE.java
@@ -0,0 +1,16 @@
+package net.cjcrispy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookOfE implements ModInitializer {
+	public static final String MOD_ID = "bookofe";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cjcrispy/BookOfEClient.java
@@ -0,0 +1,10 @@
+package net.cjcrispy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookOfEClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
