d4082b301afa752710a9edd5834a240bc923e05c
CJcrispy <49406764+CJcrispy@users.noreply.github.com>
2025-01-19 11:44:07 -0500

    Added Worm Hammer

--- src/main/java/net/cjcrispy/BookOfE.java
+++ src/main/java/net/cjcrispy/BookOfE.java
@@ -1,5 +1,6 @@
 package net.cjcrispy;
 
+import net.cjcrispy.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class BookOfE implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
