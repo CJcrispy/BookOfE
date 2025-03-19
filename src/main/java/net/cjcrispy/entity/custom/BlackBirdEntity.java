c10d102fb42fd658fad2f63fff4f015f6dbfef5d
CJcrispy <49406764+CJcrispy@users.noreply.github.com>
2025-03-19 16:21:12 -0400

    Dungeon test + mob refacotring

--- src/main/java/net/cjcrispy/entity/custom/BlackBirdEntity.java
+++ src/main/java/net/cjcrispy/entity/custom/BlackBirdEntity.java
@@ -1,5 +1,6 @@
 package net.cjcrispy.entity.custom;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -15,11 +16,13 @@ import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class BlackBirdEntity extends HostileEntity implements GeoEntity {
 
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
+    private boolean isDying = false;
 
     public BlackBirdEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
@@ -47,9 +50,10 @@ public class BlackBirdEntity extends HostileEntity implements GeoEntity {
 
     public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 50)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 10F)
+                .add(EntityAttributes.GENERIC_ARMOR, 0F)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.35)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 5F)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 10)
                 .add(EntityAttributes.GENERIC_SCALE, 1.35);
     }
@@ -58,7 +62,6 @@ public class BlackBirdEntity extends HostileEntity implements GeoEntity {
     @Override
     public void tick() {
         super.tick();
-
         if (this.getWorld().isClient()) {
             // Client-side effects, like particles or animations
         } else {
@@ -68,44 +71,56 @@ public class BlackBirdEntity extends HostileEntity implements GeoEntity {
 
     @Override
     public void onDeath(DamageSource damageSource) {
+        if (!this.isDying) {
+            this.isDying = true;
+            this.getWorld().sendEntityStatus(this, (byte) 60); // Triggers animation
+        }
         super.onDeath(damageSource);
-
-        // Example: Add a death sound or custom effects
-        this.getWorld().sendEntityStatus(this, (byte) 60); // Play the death particles
     }
 
     @Override
-    public void registerControllers(final AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 0, this::predicate));
+    public void handleStatus(byte status) {
+        if (status == 60) {
+            this.isDying = true;
+        }
+        super.handleStatus(status);
     }
 
+    private static final RawAnimation ATTACK_ANIMATION = RawAnimation.begin().thenPlay("attack");
+
+    private final AnimationController ATTACK_ANIMATION_CONTROLLER = new AnimationController<>(this, "attack_controller", state -> PlayState.STOP)
+            .triggerableAnim("attack_animation", ATTACK_ANIMATION).transitionLength(5);
+
+
+    @Override
+    public void registerControllers(final AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "controller", 5, this::predicate));
+        controllers.add(
+                DefaultAnimations.genericWalkIdleController(this).transitionLength(5),
+                ATTACK_ANIMATION_CONTROLLER,
+                DefaultAnimations.genericLivingController(this)
+        );
+    }
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
         // If the entity is dead, play the death animation and stop further updates
-        if (this.isDead()) {
+        if (this.isDying) {
             tAnimationState.getController().setAnimation(RawAnimation.begin().then("death", Animation.LoopType.PLAY_ONCE));
             return PlayState.CONTINUE; // Stop further animation updates
         }
 
-        if (this.handSwinging) {
-            tAnimationState.getController().setAnimation(RawAnimation.begin().then("attack", Animation.LoopType.PLAY_ONCE));
-            return PlayState.CONTINUE;
-        }
-
         if (tAnimationState.isMoving()) {
             tAnimationState.getController().setAnimation(
-                    RawAnimation.begin().then(this.isAggro() ? "walk_aggro" : "walk", Animation.LoopType.LOOP)
+                    RawAnimation.begin().then("walk", Animation.LoopType.LOOP)
             );
             return PlayState.CONTINUE;
         } else {
             tAnimationState.getController().setAnimation(
-                    RawAnimation.begin().then(this.isAggro() ? "idle_aggro" : "idle", Animation.LoopType.LOOP)
+                    RawAnimation.begin().then("idle", Animation.LoopType.LOOP)
             );
             return PlayState.CONTINUE;
         }
-
     }
-
 
     public boolean isAggro() {
         return this.getTarget() != null; // Aggro if the entity has a valid target
